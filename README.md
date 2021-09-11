706e262f447a9e2c4c71ec52ac353612f7915c46
Envyre-Development <86875820+Envyre-Development@users.noreply.github.com>
2021-09-11 17:20:06 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# passwd-generator
+
+Passwd_generator is an easy way to generate, hash and check passwords.
+
+**Documentation**
+
+Install the
+
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(
+  name = 'passwd_generator',         
+  packages = ['passwd_generator'], 
+  version = '0.1',     
+  license='MIT',        
+  description = 'passwd_generator is a easy way to generate passwords, hash passwords and check passwords.',   
+  author = 'Areo',                   
+  author_email = 'areo@envyre.de',      
+  url = 'https://github.com/user/reponame',  
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    
+  keywords = ['Generator', 'Password', 'passwd_generator'],   
+  classifiers=[
+    'Development Status :: 4 - Beta',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
