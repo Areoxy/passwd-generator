145c9f7b813e286168637eea74e8bc33f593b428
Envyre-Development <86875820+Envyre-Development@users.noreply.github.com>
2021-09-11 18:27:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,4 +24,6 @@ setup(
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
   ],
+  long_description=long_description,
+  long_description_content_type='text/markdown',
 )
