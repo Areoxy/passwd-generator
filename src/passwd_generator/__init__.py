9fee29b8436e6d86630bcaf69bcec4fcc7d17d0f
Luis <86875820+Areoxy@users.noreply.github.com>
2026-03-27 16:30:50 +0100

    Add files via upload

--- src/passwd_generator/__init__.py
+++ src/passwd_generator/__init__.py
@@ -1,2 +1,9 @@
-from passwd_generator import passwd
-__all__ = ['passwd']
+from .passwd_generator import passwd
+__all__ = ["passwd"]
+
+__name__ = "passwd-generator"
+__author__ = "Areo"
+__email__ = "areo@envyre.dev"
+__version__ = "2026.1.1"
+__license__ = "MIT"
+__copyright__ = f"Copyright (c) 2025 {__author__}"
